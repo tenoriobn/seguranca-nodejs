4a9f71ceebacd5abb0684acab4aea275d6734584
Bruno Tenório <tenoriobn@gmail.com>
2024-11-28 12:11:16 -0300

    cadastrando o primeiro usuário e implementando funções de CRUD do usuário

--- api/routes/usuariosRoute.js
+++ api/routes/usuariosRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const UsuarioController = require('../controllers/usuarioController');
 
 const router = Router();
 
 router
-  .post('/usuarios')
-  .get('/usuarios')
-  .get('/usuarios/id/:id')
-  .put('/usuarios/id/:id')
-  .delete('/usuarios/id/:id')
+  .post('/usuarios', UsuarioController.cadastrar)
+  .get('/usuarios', UsuarioController.buscarTodosUsuarios)
+  .get('/usuarios/id/:id', UsuarioController.buscarUsuarioPorId)
+  .put('/usuarios/id/:id', UsuarioController.editarUsuario)
+  .delete('/usuarios/id/:id', UsuarioController.deletarUsuario)
 
 module.exports = router;
