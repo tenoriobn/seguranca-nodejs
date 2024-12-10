3f8079725ea65ec028fe2ed7b28a57545f002e06
Bruno Tenório <tenoriobn@gmail.com>
2024-12-10 20:43:45 -0300

    Finalizando cadastro de perfis e permissões

--- api/services/segurancaService.js
+++ api/services/segurancaService.js
@@ -1,4 +1,5 @@
 const database = require('../models');
+const Sequelize = require('sequelize')
 
 class SegurancaService {
   async cadastrarAcl(dto) {
@@ -18,7 +19,53 @@ class SegurancaService {
       where: {
         id: dto.usuarioId
       }
-    })
+    });
+
+    if (!usuario) {
+      throw new Error("Usuário não cadastrado!");
+    };
+
+    const rolesCadastradas = await database.roles.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: dto.roles
+        }
+      }
+    });
+
+    const permissoesCadastradas = await database.permissoes.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: dto.permissoes
+        }
+      }
+    });
+
+    await usuario.removeUsuario_roles(usuario.usuario_roles);
+    await usuario.removeUsuario_permissoes(usuario.usuario_permissoes);
+
+    await usuario.addUsuario_roles(rolesCadastradas);
+    await usuario.addUsuario_permissoes(permissoesCadastradas);
+
+    const novoUsuario = await database.usuarios.findOne({
+      include: [
+        {
+          model: database.roles,
+          as: 'usuario_roles',
+          attributes: ['id', 'nome', 'descricao']
+        },
+        {
+          model: database.permissoes,
+          as: 'usuario_permissoes',
+          attributes: ['id', 'nome', 'descricao']
+        }
+      ],
+      where: {
+        id: dto.usuarioId
+      }
+    });
+
+    return novoUsuario;
   }
 }
 
